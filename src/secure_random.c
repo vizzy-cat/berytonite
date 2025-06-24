74456722f65600c3add79a359127e86efbf3d85a
vizzy-cat <azerytid@gmail.com>
2025-06-24 18:01:31 +0700

    secure_random : add android fallback

--- src/secure_random.c
+++ src/secure_random.c
@@ -4,6 +4,8 @@
 #if defined(__linux__)
 	#include <sys/random.h>
 	#include <sys/types.h>
+	#include <fcntl.h>
+	#include <unistd.h>
 #elif defined(_WIN32)
 	#include <windows.h>
 	#include <bcrypt.h>
@@ -26,10 +28,34 @@ bool krypton_secure_random(void* buffer, size_t buffer_length) {
 
 #if defined(__linux__)
 	// Linux
-	if (getrandom(buffer, buffer_length, 0) != (ssize_t)buffer_length) {
+#if defined(__ANDROID__)
+	// Android fallback
+	int fd = open("/dev/urandom", O_RDONLY);
+	if (fd < 0) {
 		krypton_set_error(KRYPTON_ERR_RAND_FAILED);
 		return false;
 	}
+
+	ssize_t total = 0;
+	while (total < (ssize_t)buffer_length) {
+		ssize_t r = read(fd, (char*)buffer + total, buffer_length - total);
+		if (r <= 0) {
+			close(fd);
+			krypton_set_error(KRYPTON_ERR_RAND_FAILED);
+			return false;
+		}
+		total += r;
+	}
+
+	close(fd);
+#else
+	int out = getrandom(buffer, buffer_length, 0);
+
+	if (out != (ssize_t)buffer_length) {
+		krypton_set_error(KRYPTON_ERR_RAND_FAILED);
+		return false;
+	}
+#endif // Android
 #elif defined(_WIN32)
 	// Windows
 	if (BCryptGenRandom(NULL, buffer, (ULONG)buffer_length, BCRYPT_USE_SYSTEM_PREFERRED_RNG) != 0) {
