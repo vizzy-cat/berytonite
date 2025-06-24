fa2cd583773be61bfd68e5002620e364df773e87
vizzy-cat <azerytid@gmail.com>
2025-06-24 18:00:35 +0700

    test : fix base64 test

--- test/base64.c
+++ test/base64.c
@@ -11,6 +11,14 @@
 		} \
 	}
 
+#define ASSERT_EQ_MEM(expected, actual, size) \
+	{ \
+		if (memcmp(expected, actual, size) != 0) { \
+			printf("FAILED : memory mismatch (size = %i)", size); \
+			return 1; \
+		} \
+	} \
+
 int main() {
 	// Base64 Encoding & Decoding Tester
 	const char* input1 = "Man";
@@ -18,7 +26,8 @@ int main() {
 	char decoded[32];
 
 	// Case 1: Encoding test
-	if (krypton_base64_encode(encoded, (void*)input1, strlen(input1)) == 0) {
+	int out = krypton_base64_encode(encoded, (void*)input1, strlen(input1));
+	if (out == 0) {
 		printf("Encode 1 failed : %i\n", krypton_get_error());
 		return 1;
 	}
@@ -26,17 +35,19 @@ int main() {
 	ASSERT_EQ_STR("TWFu", encoded);
 
 	// Case 2: Decoding test
-	if (krypton_base64_decode((void*)decoded, encoded, strlen(encoded)) == 0) {
+	out = krypton_base64_decode((void*)decoded, encoded, strlen(encoded));
+	if (out == 0) {
 		printf("Decode 2 failed : %i\n", krypton_get_error());
 		return 1;
 	}
 
-	ASSERT_EQ_STR("Man", decoded);
+	ASSERT_EQ_MEM("Man", decoded, 3);
 
 	// Case 3: Encoder padding test (1 byte)
 	const char* input2 = "M";
 
-	if (krypton_base64_encode(encoded, (void*)input2, strlen(input2)) == 0) {
+	out = krypton_base64_encode(encoded, (void*)input2, strlen(input2));
+	if (out == 0) {
 		printf("Encode 3 failed : %i\n", krypton_get_error());
 		return 1;
 	}
@@ -44,12 +55,13 @@ int main() {
 	ASSERT_EQ_STR("TQ==", encoded);
 
 	// Case 4: Decoder padding test (1 byte)
-	if (krypton_base64_decode(decoded, encoded, strlen(encoded)) == 0) {
+	out = krypton_base64_decode(decoded, encoded, strlen(encoded));
+	if (out == 0) {
 		printf("Decode 4 failed : %i\n", krypton_get_error());
 		return 1;
 	}
 
-	ASSERT_EQ_STR("M", decoded);
+	ASSERT_EQ_MEM("M", decoded, 1);
 
 	printf("All Base64 tests passed.\n");
 	return 0;
