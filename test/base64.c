7fca6d9f9bbce8cd91f8b97eb1e1de399d44256e
vizzy-cat <azerytid@gmail.com>
2025-06-27 15:22:40 +0700

    test : remove error handling

--- test/base64.c
+++ test/base64.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <string.h>
 #include "beryton/base64.h"
-#include "beryton/error.h"
 
 #define ASSERT_EQ_STR(expected, actual) \
 	{ \
@@ -26,40 +25,24 @@ int main() {
 	char decoded[32];
 
 	// Case 1: Encoding test
-	int out = beryton_base64_encode(encoded, (void*)input1, strlen(input1));
-	if (out == 0) {
-		printf("Encode 1 failed : %i\n", beryton_get_error());
-		return 1;
-	}
+	beryton_base64_encode(encoded, (void*)input1, strlen(input1));
 
 	ASSERT_EQ_STR("TWFu", encoded);
 
 	// Case 2: Decoding test
-	out = beryton_base64_decode((void*)decoded, encoded, strlen(encoded));
-	if (out == 0) {
-		printf("Decode 2 failed : %i\n", beryton_get_error());
-		return 1;
-	}
+	beryton_base64_decode((void*)decoded, encoded, strlen(encoded));
 
 	ASSERT_EQ_MEM("Man", decoded, 3);
 
 	// Case 3: Encoder padding test (1 byte)
 	const char* input2 = "M";
 
-	out = beryton_base64_encode(encoded, (void*)input2, strlen(input2));
-	if (out == 0) {
-		printf("Encode 3 failed : %i\n", beryton_get_error());
-		return 1;
-	}
+	beryton_base64_encode(encoded, (void*)input2, strlen(input2));
 
 	ASSERT_EQ_STR("TQ==", encoded);
 
 	// Case 4: Decoder padding test (1 byte)
-	out = beryton_base64_decode(decoded, encoded, strlen(encoded));
-	if (out == 0) {
-		printf("Decode 4 failed : %i\n", beryton_get_error());
-		return 1;
-	}
+	beryton_base64_decode(decoded, encoded, strlen(encoded));
 
 	ASSERT_EQ_MEM("M", decoded, 1);
 
