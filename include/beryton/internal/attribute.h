3576c357c87ff37fa960be156335f599999b0184
vizzy-cat <azerytid@gmail.com>
2025-06-28 13:35:09 +0700

    Internal : Remove ZERO_OPTIMIZE_ATT

--- include/beryton/internal/attribute.h
+++ include/beryton/internal/attribute.h
@@ -5,7 +5,6 @@
 #if defined(__GNUC__)||defined(__clang__)
 	#define PURE_ATT __attribute__((pure))
 	#define INLINE_ATT __attribute__((always_inline))
-	#define ZERO_OPTIMIZE_ATT __attribute__((optimize("O0")))
 #else
 	#define PURE_ATT
 	#define INLINE_ATT
--- src/util.c
+++ src/util.c
@@ -14,7 +14,7 @@ void beryton_bytetohex(char* out, const uint8_t* in, size_t len) {
 	out[len * 2] = '\0';
 }
 
-ZERO_OPTIMIZE_ATT void beryton_memzero(void* ptr, size_t len) {
+void beryton_memzero(void* ptr, size_t len) {
 #if defined(__GNUC__)||defined(__clang__)
 	volatile unsigned char* p = (volatile unsigned char*)ptr;
 	while (len--) *p++ = 0;
