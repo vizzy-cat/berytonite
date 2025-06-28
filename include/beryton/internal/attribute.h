b61f5b49f086cee121f2fe6f4872d948a7e79999
vizzy-cat <azerytid@gmail.com>
2025-06-28 10:08:00 +0700

    Internal : Add new attribute

--- include/beryton/internal/attribute.h
+++ include/beryton/internal/attribute.h
@@ -3,7 +3,9 @@
 #pragma once
 
 #if defined(__GNUC__)||defined(__clang__)
-   #define PURE_ATT __attribute__((pure))
+	#define PURE_ATT __attribute__((pure))
+	#define INLINE_ATT __attribute__((always_inline))
 #else
-   #define PURE_ATT
+	#define PURE_ATT
+	#define INLINE_ATT
 #endif
