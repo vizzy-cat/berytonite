c28676cbe8c57335742412c78a5907a67189e3cd
vizzy-cat <azerytid@gmail.com>
2025-06-28 12:59:30 +0700

    Util : Add Secure Memory Erase

--- src/util.c
+++ src/util.c
@@ -1,6 +1,11 @@
 #include "util.h"
+#include "internal/attribute.h"
 #include <stdio.h>
 #include <string.h>
+
+#ifdef _MSC_VER
+	#include <windows.h>
+#endif
 
 void beryton_bytetohex(char* out, const uint8_t* in, size_t len) {
 	for (size_t i = 0; i < len; i++) {
@@ -8,3 +13,17 @@ void beryton_bytetohex(char* out, const uint8_t* in, size_t len) {
 	}
 	out[len * 2] = '\0';
 }
+
+ZERO_OPTIMIZE_ATT void beryton_memzero(void* ptr, size_t len) {
+#if defined(__GNUC__)||defined(__clang__)
+	volatile unsigned char* p = (volatile unsigned char*)ptr;
+	while (len--) *p++ = 0;
+#elif defined(_MSC_VER)
+	SecureZeroMemory(ptr, len);
+#else
+	memset(ptr, 0, len);
+
+	asm volatile ("" : : "r"(ptr) : "memory");
+#endif
+}
+
