262764e3f5bf797394259b011eae89f9997db2d3
azerytid@gmail.com <azerytid@gmail.com>
2025-11-02 13:25:39 +0700

    util.c : Change function's name

--- src/util.c
+++ src/util.c
@@ -7,14 +7,14 @@
 	#include <windows.h>
 #endif
 
-void beryton_bytetohex(char* out, const uint8_t* in, size_t len) {
+void bt_bytetohex(char* out, const uint8_t* in, size_t len) {
 	for (size_t i = 0; i < len; i++) {
 		sprintf(&out[i * 2], "%02x", in[i]);
 	}
 	out[len * 2] = '\0';
 }
 
-void beryton_memzero(void* ptr, size_t len) {
+void bt_memzero(void* ptr, size_t len) {
 #if defined(__GNUC__)||defined(__clang__)
 	volatile unsigned char* p = (volatile unsigned char*)ptr;
 	while (len--) *p++ = 0;
