4c06a73de9cef411cdd254885d73c5fb8ea55b00
vizzy-cat <azerytid@gmail.com>
2025-12-07 21:58:47 +0700

    stream.c : add parameter checker and move bt_reuse to stream.h

--- src/stream.c
+++ src/stream.c
@@ -4,6 +4,11 @@
 
 // init
 void bt_init(bt_ctx* ctx, bt_algo* algo) {
+	// check parameter
+	if (!ctx || !algo) {
+		return;
+	}
+
 	// allocate new memory for internal ctx and insert the algorithm into the ctx
 	ctx->algo_ctx = (void*)malloc(algo->ctx_size);
 	ctx->algo = algo;
@@ -14,27 +19,35 @@ void bt_init(bt_ctx* ctx, bt_algo* algo) {
 
 // update
 void bt_update(bt_ctx* ctx, const uint8_t* in, size_t len, uint8_t* out /* some algorithm might not give anything */) {
+	// check parameter
+	if (!ctx || !in || !len) {
+		return;
+	}
+
 	// update the ctx
 	ctx->algo->update(ctx->algo_ctx, in, len, out);
 }
 
 // final
 void bt_final(bt_ctx* ctx, uint8_t* out) {
+	// check parameter
+	if (!ctx || !out) {
+		return;
+	}
+
 	// finalize the ctx and give the result
 	ctx->algo->final(ctx->algo_ctx, out);
 }
 
 // free
 void bt_free(bt_ctx* ctx) {
+	// check parameter
+	if (!ctx) {
+		return;
+	}
+
 	// free the internal ctx
 	free(ctx->algo_ctx);
-}
-
-// reuse
-inline void bt_reuse(bt_ctx* ctx) {
-	// free and allocate new memory for the internal ctx
-	bt_free(ctx);
-	bt_init(ctx, ctx->algo);
 }
 
 // why do we even need these comments?
