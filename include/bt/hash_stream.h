4183daf5c66921a2227a2763ca33d120815a2520
vizzy-cat <azerytid@gmail.com>
2026-01-07 18:36:16 +0700

    stream : basic hash-only generic stream implementation

---
+++ include/bt/hash_stream.h
@@ -0,0 +1,31 @@
+// berytonite universal hash stream
+
+#pragma once
+
+#include <stdint.h>
+#include <stddef.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct bt_hash bt_hash;
+
+typedef struct {
+	void* internal_ctx;
+	bt_hash* hash_algo;
+} bt_hash_ctx;
+
+void bt_hash_init(bt_hash_ctx* ctx, const bt_hash* hash_algo);
+void bt_hash_update(bt_hash_ctx* restrict ctx, const uint8_t* restrict in, size_t len);
+void bt_hash_final(bt_hash_ctx* ctx, uint8_t* out);
+void bt_hash_free(bt_hash_ctx* ctx);
+
+static inline void bt_hash_reuse(bt_hash_ctx* ctx, bt_hash* hash_algo) {
+	bt_hash_free(ctx);
+	bt_hash_init(ctx, hash_algo);
+}
+
+#ifdef __cplusplus
+}
+#endif
---
+++ src/hash_stream.c
@@ -0,0 +1,30 @@
+#include "hash_stream.h"
+#include "internal/stream.h"
+#include <stdlib.h>
+
+void bt_hash_init(bt_hash_ctx* ctx, const bt_hash* hash_algo) {
+	if (!ctx || !hash_algo) return;
+
+	ctx->internal_ctx = malloc(hash_algo->ctx_size);
+	ctx->hash_algo = hash_algo;
+
+	hash_algo->init(ctx->internal_ctx);
+}
+
+void bt_hash_update(bt_hash_ctx* restrict ctx, const uint8_t* restrict in, size_t len) {
+	if(!ctx || !in || !len) return;
+
+	ctx->hash_algo->update(ctx->internal_ctx, in, len);
+}
+
+void bt_hash_final(bt_hash_ctx* ctx, uint8_t* out) {
+	if (!ctx || !out) return;
+
+	ctx->hash_algo->final(ctx->internal_ctx, out);
+}
+
+void bt_hash_free(bt_hash_ctx* ctx) {
+	if (!ctx) return;
+
+	free(ctx->internal_ctx);
+}
